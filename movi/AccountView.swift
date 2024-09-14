9e6d945596f80e4cdb69546a4b30005d45eb8a83
Alonso Huerta <alonso12_284@outlook.com>
2024-09-14 15:13:01 -0600

    Initial Commit

---
+++ movi/AccountView.swift
@@ -0,0 +1,18 @@
+//
+//  AccountView.swift
+//  movi
+//
+//  Created by Alonso Huerta on 14/09/24.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccountView()
+}
---
+++ movi/eCheqView.swift
@@ -0,0 +1,62 @@
+//
+//  eCheqView.swift
+//  movi
+//
+//  Created by Alonso Huerta on 14/09/24.
+//
+
+import SwiftUI
+
+struct eCheqView: View {
+    @State private var activeSheet: ActiveSheet?
+
+    enum ActiveSheet: Identifiable {
+        case send, receive, transaction
+
+        var id: Int {
+            hashValue
+        }
+    }
+
+    var body: some View {
+        NavigationStack {
+            List {
+                Section("eCheq Actions") {
+                    HStack(alignment: .center) {
+                        Button(action: {
+                            activeSheet = .send
+                        }, label: { Label("Send", systemImage: "square.and.arrow.up") })
+                        .fixedSize()
+
+                        Spacer()
+                        Divider()
+                        Spacer()
+
+                        Button(action: {
+                            activeSheet = .receive
+                        }, label: { Label("Receive", systemImage: "square.and.arrow.down") })
+                        .fixedSize()
+                    }
+                    .padding()
+                }
+                Section("eCheqs") {
+
+                }
+            }
+        }
+        .sheet(item: $activeSheet) { sheet in
+            switch sheet {
+            case .send:
+                Text("Send")
+            case .receive:
+                Text("Receive")
+            case .transaction:
+                Text("Transaction View")
+            }
+        }
+    }
+}
+
+#Preview {
+    eCheqView()
+}
